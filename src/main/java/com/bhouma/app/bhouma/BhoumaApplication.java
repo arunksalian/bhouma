39fcb16f0a38b43583412a6a51241fc5e01e8529
Arun Kannan <arkannan@ghx.com>
2019-08-08 12:36:37 +0530

    Added source folder.

---
+++ src/main/java/com/bhouma/app/bhouma/BhoumaApplication.java
@@ -0,0 +1,13 @@
+package com.bhouma.app.bhouma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhoumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhoumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhouma/app/bhouma/BhoumaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bhouma.app.bhouma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BhoumaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
